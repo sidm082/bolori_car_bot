3025238504fc28dc561ab210177b9d7507f62d30
sidm082 <sajjad.bakhshian.org@gmail.com>
2025-05-08 13:04:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1214,9 +1214,7 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        loop = asyncio.get_event_loop()
-        loop.create_task(main())
-        loop.run_forever()
+        asyncio.run(main())
     except Exception as e:
         logger.error(f"خطا در راه‌اندازی ربات: {e}")
         raise e
